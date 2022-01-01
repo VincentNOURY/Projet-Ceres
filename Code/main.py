06558846ba5f564483857e3e948148d259e87ced
Vincent NOURY <vincent.noury@efrei.net>
2022-01-01 22:59:51 +0100

    Fixed some typos, added threading for web server and added while True for with 1 hour sleep delay

--- Code/main.py
+++ Code/main.py
@@ -1,14 +1,18 @@
-from Meteo.meteo import get_meteo
+from WebServer.web_server import start as start_server
 from Meteo.meteo import write_to_json
 from Meteo.meteo import diff_dates
-from datetime import date
+from Meteo.meteo import get_meteo
 from datetime import timedelta
-from WebServer.web_server import start
+from datetime import date
+from time import sleep
+import threading
+import json
 
 
 SEUIL = 3
 ATROPLU = 10
 TROUMID = 30
+DELAY = 3600 # time between checks
 
 def calcul_arrosage(dernier_arrosage : str):
     """
@@ -19,25 +23,26 @@ def calcul_arrosage(dernier_arrosage : str):
 
     Return :
     """
+    print("[Debug] Start of calcul_arrosage")
     with open("WebServer/templates/pluie.json", 'r') as file:
         s=0
         pluie = json.loads(file.read())
         for i in pluie.keys():
-            for j in i.keys():
+            for j in pluie[i].keys():
                 s = s + int(pluie[i][j])
-        if diff_dates(dernier_arrosage)>=SEUIL:
-            if s<ATROPLU:
-                pass #arroser
     h=0
     a=0
     with open("WebServer/Capteurs info/Capteurs.json") as file:
         humidite = json.loads(file.read())
         for i in humidite.keys():
-            for j in i.keys():
-                h = h + int(humidite[i]["humidite"])
+            h = h + int(humidite[i]["humidity"])
         a = h/len(humidite.keys())
-        if a<TROUMID:
-            pass #arroser
+
+    if diff_dates(dernier_arrosage)>=SEUIL:
+        if s < ATROPLU and a < TROUMID:
+            print("[Debug] c bon")
+            dernier_arrosage = str(date.today())
+            #pass #arroser
 
 def creer_date(nb_days):
     return(str(date.today() + timedelta(days=1)))
@@ -46,7 +51,12 @@ def prevision_arrosage(dernier_arrosage):
     return creer_date(SEUIL - diff_dates(dernier_arrosage))
 
 if __name__ == '__main__':
-    dernier_arrosage = "2021-11-28"
-    estimation_arrosage = prevision_arrosage(dernier_arrosage)
-    pluie, temp = write_to_json("WebServer/templates")
-    start()
+    server_thread = threading.Thread(target=start_server)
+    server_thread.start()
+
+    while True:
+        dernier_arrosage = "2021-11-28"
+        estimation_arrosage = prevision_arrosage(dernier_arrosage)
+        pluie, temp = write_to_json("WebServer/templates")
+        calcul_arrosage(dernier_arrosage)
+        sleep(DELAY)
